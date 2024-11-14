c005c24d5566e10cf6e23897efcbe7c7a3346d5f
lizethalonso <lizethalsa23@gmail.com>
2024-11-14 17:42:42 -0500

    Rama para deploy

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -101,6 +101,7 @@ const Modal = ({ isOpen, onClose, producto }) => {
                 />
 
                 {/* Categorías */}
+                <div className="text-xl text-gray-600">Características</div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-3 mb-4">
                   <div className="flex items-center gap-2 px-3 py-2 bg-gray-100 rounded-lg text-sm sm:text-base justify-center border-gray-400 border-2">
                     <BsRulers className="text-xl" />
