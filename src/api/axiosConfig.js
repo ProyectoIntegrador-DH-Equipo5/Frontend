8700d36d359b85ae15d526ee6502c526096681c6
lizethalonso <lizethalsa23@gmail.com>
2024-12-06 23:32:35 -0500

    Ensayo2

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// export const axiosConfig = axios.create({
-//     baseURL: "http://localhost:8080",
-// });
+console.log('API URL:', process.env.REACT_APP_API_URL || "http://localhost:8080");
 
 export const axiosConfig = axios.create({
     baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
