b4c81d7cfeb6ecef4605afc5d5cd2544ca2d9089
lizethalonso <lizethalsa23@gmail.com>
2024-12-06 21:53:57 -0500

    Ensayo deploy

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// export const axiosConfig = axios.create({
+//     baseURL: "http://localhost:8080",
+// });
+
 export const axiosConfig = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
 });
 
 // Interceptor para incluir el token en cada solicitud
